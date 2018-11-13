ff0d93e0829c3d17e92f8de32db5852334d8cc64
амир абдрахманов <Uncle205@mail.ru>
2018-11-13 19:29:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Балжигитов Бекдос", 2, 4, Gender.male, EducationForm.Found, 200000));
+            students.Add(new Student("Нариман Нарымбеков ", 1, 5, Gender.male, EducationForm.Direct, 100000 ));
+            students.Add(new Student("Арений Культиков", 3, 3, Gender.male, EducationForm.Found, 5444000));
+
+            int minSalary = 100000;
+
+            var grant = students.Where(a => ((a.FamilySalary<= minSalary) || ( a.FamilySalary <= minSalary)) && a.EducationForm == EducationForm.Direct).OrderByDescending(a => a.Gpa);
+            foreach (var grants in grant)
+            {
+                Console.WriteLine("ФИО: " + grants.name);
+                Console.WriteLine("Пол: " + grants.Gender);
+                Console.WriteLine("Группа: " + grants.group);
+                Console.WriteLine("Средняя оценка: " + grants.Gpa);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp9/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public enum Gender {male,female }
+    public enum EducationForm { Direct,Found}
+
+    public class Student
+    {
+        public string name { get; set; }
+        public int group { get; set; }
+        public int Gpa { get; set; }
+        public Gender Gender { get; set; }
+        public EducationForm EducationForm { get; set; }
+        public int FamilySalary { get; set; }
+
+        public Student(string Name,int group,int gpa,Gender gender,EducationForm educationForm,  int familySalary)
+        {
+            name = Name;
+            Gpa = gpa;
+            Gender = gender;
+            EducationForm = educationForm;
+            FamilySalary = familySalary;
+        }
+    }
+}
